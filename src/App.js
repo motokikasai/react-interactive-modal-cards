9c43c615e8eb12c008c7a4a0e8e980fe656d6c67
motokikasai <kmomoky@hotmail.com>
2020-04-06 22:27:01 +0200

    Create footer, style header component add basic tpl for Modal component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from "react";
 import "./styles/App.css";
+import data from "../src/employees.json";
 import Hero from "./components/hero/";
 
 function App() {
+  const numOfMembers = data.length;
+
   return (
     <div className="App">
       <Hero />
-      <h3>
-        Our team of "#" strategists, designers, managers, developers make custom
-        products for startups and leading brands.
-      </h3>
+      <section className="main-content">
+        <h2>
+          Our team of<span className="numOfMembers">{numOfMembers}</span>
+          strategists, designers, managers, developers make custom products for
+          startups and leading brands.
+        </h2>
+      </section>
+      <footer>
+        <div>
+          Icons made by{" "}
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik
+          </a>{" "}
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
