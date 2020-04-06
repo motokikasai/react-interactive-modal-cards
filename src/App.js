5c7b6429f03d926294f9c736869b1858afd9346d
motokikasai <kmomoky@hotmail.com>
2020-04-06 23:28:08 +0200

    List all the cards onload via map

--- src/App.js
+++ src/App.js
@@ -10,11 +10,32 @@ function App() {
     <div className="App">
       <Hero />
       <section className="main-content">
-        <h2>
+        <h2 className="main-subheader">
           Our team of<span className="numOfMembers">{numOfMembers}</span>
           strategists, designers, managers, developers make custom products for
           startups and leading brands.
         </h2>
+        <main>
+          {data.map((member, key) => {
+            return (
+              <div className="card" key={member.id}>
+                <div className="bg-dark"></div>
+                <div className="card-content">
+                  <img
+                    className="avatar"
+                    src={member.avatar}
+                    alt={member.firstName}
+                  />
+                  <div className="full-name">
+                    <span>{member.firstName} </span>
+                    <span>{member.lastName}</span>
+                  </div>
+                  <div>{member.jobTitle}</div>
+                </div>
+              </div>
+            );
+          })}
+        </main>
       </section>
       <footer>
         <div>
